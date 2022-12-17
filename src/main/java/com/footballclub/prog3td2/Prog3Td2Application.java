61848c21ff3513c1de07764e3265696f8cdfb0d2
Micael Jerry <hei.jerry.2@gmail.com>
2022-12-17 15:19:21 +0300

    initial commit

---
+++ src/main/java/com/footballclub/prog3td2/Prog3Td2Application.java
@@ -0,0 +1,13 @@
+package com.footballclub.prog3td2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prog3Td2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Prog3Td2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/footballclub/prog3td2/Prog3Td2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.footballclub.prog3td2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prog3Td2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
