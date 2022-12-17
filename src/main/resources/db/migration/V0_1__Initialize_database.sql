c7e51529150ff8e76b2773bc6f4dc429cdbb0c76
Micael Jerry <hei.jerry.2@gmail.com>
2022-12-17 21:44:17 +0300

    fix: sql scripts implementation of MPD

---
+++ src/main/resources/db/migration/V0_1__Initialize_database.sql
@@ -0,0 +1,39 @@
+create table team
+(
+    id   integer primary key,
+    name varchar(80) not null
+        constraint team_name_unique unique
+);
+
+create table player
+(
+    id      integer primary key,
+    team_id integer references team ("id"),
+    name    varchar(100),
+    number  integer
+        constraint player_number_limitation check ( number > 0 and number < 100)
+);
+
+create table sponsor
+(
+    id   integer primary key,
+    name varchar(100) not null
+        constraint sponsor_name_unique unique
+);
+
+create table have
+(
+    team_id    integer references team ("id"),
+    sponsor_id integer references sponsor ("id"),
+    primary key (team_id, sponsor_id)
+);
+
+create table play_again
+(
+    id        integer primary key,
+    team_id_1 integer references team ("id") not null
+        constraint team_play_again check ( team_id_1 != team_id_2 ),
+    team_id_2 integer references team ("id") not null,
+    datetime  date,
+    stadium   varchar(100)
+);
